138e2d9eb7a830d4c145112e4a8e62b8c2651448
slizyoke <55554740+slizyoke@users.noreply.github.com>
2020-04-30 14:14:36 +0300

    Update aswq.php

--- Elegance_Mobilya/aswq.php
+++ Elegance_Mobilya/aswq.php
@@ -1,19 +1,19 @@
-https://docs.google.com/spreadsheets/d/15NWzTKBiZdSO-6RVzAWPmFJFG_h1AFj09XbPFwv-9I4/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1E_h2njfEQrxlJG-_y-0fM448ZSHgV7Fv4G9xf_8mFIo/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/11uh8nHMRhUohCuprEdUt79Ofpi4kbaZ7UPWk0xL7JQ8/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1iZqvtwS42oPiVELDNESLLrsNtJaZeWdKDDlLe6DfYt4/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1wRS69qAVHJee9ha2ODrRo-dE6SxmcbIvGXsEJviM9Tg/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/18MsnLGGmYR9cAbyyisWrDjSsIj_lvfJe6xsb7mTUTaY/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1Ptk3lTuf0oDTd3zxmoNxSxhz-J-5JNlwziWSCIG9GFk/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1qbEexXypabhTw-L-WdYpIhPkakHX-z56qt-O647P2VE/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1WaCJnePgqThXq5F4QU9vBzCKYVau-j5da9k922FQxPw/edit?usp=sharing
-
-https://docs.google.com/spreadsheets/d/1PLzyoE0XCQasw7cdmCmTj74kQBbrNiWIwf4qMnZeaMs/edit?usp=sharing
+<a rel="external" href="https://docs.google.com/spreadsheets/d/15NWzTKBiZdSO-6RVzAWPmFJFG_h1AFj09XbPFwv-9I4/edit?usp=sharing">Kaynak 1</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1E_h2njfEQrxlJG-_y-0fM448ZSHgV7Fv4G9xf_8mFIo/edit?usp=sharing">Kaynak 2</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/11uh8nHMRhUohCuprEdUt79Ofpi4kbaZ7UPWk0xL7JQ8/edit?usp=sharing">Kaynak 3</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1iZqvtwS42oPiVELDNESLLrsNtJaZeWdKDDlLe6DfYt4/edit?usp=sharing">Kaynak 4</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1wRS69qAVHJee9ha2ODrRo-dE6SxmcbIvGXsEJviM9Tg/edit?usp=sharing">Kaynak 5</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/18MsnLGGmYR9cAbyyisWrDjSsIj_lvfJe6xsb7mTUTaY/edit?usp=sharing">Kaynak 6</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1Ptk3lTuf0oDTd3zxmoNxSxhz-J-5JNlwziWSCIG9GFk/edit?usp=sharing">Kaynak 7</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1qbEexXypabhTw-L-WdYpIhPkakHX-z56qt-O647P2VE/edit?usp=sharing">Kaynak 8</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1WaCJnePgqThXq5F4QU9vBzCKYVau-j5da9k922FQxPw/edit?usp=sharing">Kaynak 9</a>
+<br>
+<a rel="external" href="https://docs.google.com/spreadsheets/d/1PLzyoE0XCQasw7cdmCmTj74kQBbrNiWIwf4qMnZeaMs/edit?usp=sharing">Kaynak 10</a>
